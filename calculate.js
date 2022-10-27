9c5de94e245b3c91d535a3188abd26db2a6ff6d4
Nathan-Matthews <nate.matthews24@gmail.com>
2022-10-27 10:27:55 -0400

    Added backspace

--- calculate.js
+++ calculate.js
@@ -54,6 +54,22 @@ const equals = document.querySelector('.equals');
 const values = document.querySelector('.values');
 const clear = document.querySelector('.clear');
 const ops = document.querySelectorAll('.op');
+const back = document.querySelector('.backspace');
+
+// Event listener for backspace
+back.addEventListener("click",() => {
+
+    if(operator == ''){
+        firstValue = firstValue.substring(0, firstValue.length - 1);
+        updateDisplay(firstValue);
+    }
+
+    else{
+        secondValue = secondValue.substring(0, secondValue.length - 1);
+        updateDisplay(secondValue);
+    }
+});
+
 
 // Clear the display and variables on 'clear'
 clear.addEventListener("click",() => {
@@ -66,7 +82,7 @@ clear.addEventListener("click",() => {
 // Event listener for equals sign
 equals.addEventListener("click",() => {
     // If equals is pressed before an operator, ignore it.
-    if(operator == ''){
+    if(operator == ''  || secondValue == ''){
         return;
     }
     firstValue = operate(firstValue, operator, secondValue);
@@ -108,6 +124,9 @@ ops.forEach((op) => {
             // Perform an operation the same as equals but update with the new op.
             secondValue = '';
             operator = (op.textContent.charAt(0));
+        }
+        if (firstValue == ''){
+            firstValue = values.textContent;
         }
         operator = (op.textContent.charAt(0));
     
